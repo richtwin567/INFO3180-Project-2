c7498758c0993fafda12461b3f96a830a92bdbd7
DangaRanga <jasonblue1626@gmail.com>
2021-04-23 16:29:55 -0500

    Slight modification to config

--- app/config.py
+++ app/config.py
@@ -3,19 +3,28 @@ import os
 
 load_dotenv()
 
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or 'se%^&!70DCRETk*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DB_URL") or 'postgresql://yourusername:yourpassword@localhost/databasename'    
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://", 1)
+
     DEBUG = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_LIFESPAN = {'hours': 24}
     JWT_REFRESH_LIFESPAN = {'days': 30}
     UPLOAD_FOLDER = "uploads"
-    
+
+
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
     DEVELOPMENT = True
     DEBUG = True
+
 
 class ProductionConfig(Config):
     """Production Config that extends the Base Config Object"""
