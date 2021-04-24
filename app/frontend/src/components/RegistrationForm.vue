ef1643e390e0d0f11286682faae16a8c54a9b252
DangaRanga <jasonblue1626@gmail.com>
2021-04-24 02:11:07 -0500

    Switched to flask WTF for server side validation and temporarily removed csrf

---
+++ app/frontend/src/components/RegistrationForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <section id="registration-form-container">
+    <form
+      @submit.prevent="handleSubmit"
+      method="POST"
+      enctype="multipart/form-data"
+    ></form>
+  </section>
+</template>
+
+<script>
+// import * as authService from "@/services/auth.service.js";
+
+export default {
+  name: "RegistrationForm",
+  // Keeps track of component state
+  data() {
+    return {
+      username: "",
+      password: "",
+      email: "",
+      location: "",
+      biography: "",
+      photo_path: "",
+    };
+  },
+  methods: {
+    async handleSubmit(e) {
+      e.preventDefault();
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- app/frontend/src/views/RegistrationPage.vue
+++ app/frontend/src/views/RegistrationPage.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="registration">
-    Registration
+    <h1>Register New User</h1>
+    <registration-form></registration-form>
   </div>
 </template>
 
 <script>
+// Imports
+import RegistrationForm from "@/components/RegistrationForm.vue";
+
 export default {
+  components: { RegistrationForm },
   name: "RegistrationPage",
 };
 </script>
-<style scoped></style>
+<style scoped>
+#registration {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
