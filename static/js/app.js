55b5d9a051d65fc110d41285c54643e8ce3eb229
DangaRanga <jasonblue1626@gmail.com>
2021-04-24 03:36:33 -0500

    Started refactoring tobe like the lab

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,71 @@
 /* Add your Application JavaScript */
 // Instantiate our main Vue Instance
+
+const API_ENDPOINT = "http://localhost:9090/api";
+async function login(userObj) {
+  let requestParams = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: userObj.username,
+      password: userObj.password,
+    }),
+  };
+
+  return fetch(`${API_ENDPOINT}/auth/login`, requestParams)
+    .then((response) => {
+      if (!response.ok) {
+        // Error handling for when the user isn't authenticated
+        if (response.status === 401) {
+          return response.json();
+        }
+      } else if (response.data.token) {
+        // Store the JWT if its in the response
+        localStorage.setItem("jwt");
+        return response.data;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// async function register(userObj) {}
+
+async function logout() {
+  // Remove the JWT
+  localStorage.removeItem("jwt");
+
+  // Retrieve response from server
+  return axios.post(`${API_ENDPOINT}/logout`, {}).then((response) => {
+    return response.data;
+  });
+}
+
+/**
+ * Checks if the response is valid and logs the user out if its invalid
+ * @param {Response} response
+ */
+async function handleResponse(response) {
+  return response.text().then((text) => {
+    const data = text && JSON.parse(text);
+    if (!response.ok) {
+      if (response.status === 401) {
+        // Auto logout if 401 response returned from api
+        logout();
+      }
+
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+}
+
 const app = Vue.createApp({
   data() {
     return {};
@@ -111,6 +177,57 @@ const Home = {
     `,
   data() {
     return {};
+  },
+};
+
+const Login = {
+  name: "Login",
+  template: `
+  <div id="login">
+    <h1>Log in to your account</h1>
+    <login-form></login-form>
+  </div>
+  `,
+};
+
+const LoginForm = {
+  name: "LoginForm",
+  template: `
+  <section id="login-form-container">
+    <form @submit.prevent="handleSubmit" method="POST">
+        <label for="username"> Username </label>
+        <input id="username" type="text" v-model="username" required />
+        <label for="password"> Password </label>
+        <input id="password" type="password" v-model="password" required />
+        <button class="auth-form-btn" type="submit">
+          Login
+        </button>
+        <div v-if="error" class="error">
+          {{ error }}
+        </div>
+    </form>
+</section>
+  `,
+  data() {
+    return {
+      username: "",
+      password: "",
+      error: "",
+    };
+  },
+  methods: {
+    async handleSubmit(e) {
+      e.preventDefault();
+      let userObj = { username: this.username, password: this.password };
+
+      // Log the user in
+      let data = await login(userObj);
+      if (data.error) {
+        console.log(data.error);
+        this.error = data.error;
+      }
+      console.log(data);
+    },
   },
 };
 
